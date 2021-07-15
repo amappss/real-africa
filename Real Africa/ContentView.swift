6997ace070198618f39e49888d45fba56df80991
arsalan <arsalansky58@gmail.com>
2021-07-15 11:01:46 +0430

    project initilzed and resources added

---
+++ Real Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Real Africa
+//
+//  Created by Arsalan majlesi on 7/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Real Africa/Real_AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_AfricaApp.swift
+//  Real Africa
+//
+//  Created by Arsalan majlesi on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Real_AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
