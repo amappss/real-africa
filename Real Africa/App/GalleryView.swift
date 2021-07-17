470332c151340f4bd6b0534418ab5aa0696b2345
arsalan <arsalansky58@gmail.com>
2021-07-17 18:19:10 +0430

    Gallery screen grid & preview added

--- Real Africa/App/GalleryView.swift
+++ Real Africa/App/GalleryView.swift
@@ -8,12 +8,50 @@
 import SwiftUI
 
 struct GalleryView: View {
+    @State private var selectedImg = ANIMALS[0].image
+    @State private var gridColumns: Float = 3.0
+    
+    @State var gridLayout: [GridItem] = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+        
+    
+    func gridSwitched(){
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumns))
+    }
+    
     var body: some View {
         ScrollView(){
             
+            VStack(spacing:15) {
+                Image(selectedImg)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white,lineWidth: 3))
+                
+                Slider(value: $gridColumns, in: 2...4,step:1) {
+                    
+                }.onChange(of: gridColumns, perform: { value in
+                    gridSwitched()
+                })
+                
+                
+                LazyVGrid(columns:gridLayout,spacing:20) {
+                    ForEach(ANIMALS) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white,lineWidth: 1))
+                            .onTapGesture {
+                                selectedImg = animal.image
+                            }
+                    }
+                }
+            }
         }
         .frame(width: .infinity, height: .infinity, alignment: .center)
         .background(GalleryAnimationView())
+        .ignoresSafeArea(edges: .top)
     }
 }
 
