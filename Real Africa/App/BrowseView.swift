3286d6ce58cf6cae5c73ef78fdf081c55d754cf8
arsalan <arsalansky58@gmail.com>
2021-07-18 15:37:30 +0430

    Browse screen grid view feature added

--- Real Africa/App/BrowseView.swift
+++ Real Africa/App/BrowseView.swift
@@ -7,23 +7,87 @@
 
 import SwiftUI
 
+enum GridType : Int{
+    case twoGrid = 2
+    case     threeGrid = 3
+     case    oneGrid = 1
+}
 struct BrowseView: View {
+    @State private var isGridActive = false
+    @State var gridType :GridType = .twoGrid
+    @State var gridImageName = "square.grid.2x2"
     let coverImages = COVER_IMAGES
     let animals = ANIMALS
     
+    let impact = UIImpactFeedbackGenerator(style: .medium)
+    
+    func switchGrid() {
+        if(isGridActive == false){
+            isGridActive = true
+            gridType = .twoGrid
+            gridImageName = "square.grid.3x2"
+            return
+        }
+        switch gridType {
+        case .twoGrid :
+                gridType = .threeGrid
+            gridImageName = "rectangle.grid.1x2"
+        case .threeGrid :
+            gridType = .oneGrid
+            gridImageName = "square.grid.2x2"
+        default :
+            gridType = .twoGrid
+            gridImageName = "square.grid.3x2"
+        }
+        impact.impactOccurred()
+    }
+    
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView(images: coverImages)
-                    .frame(height:300)
-                    .listRowInsets(.none)
-                ForEach(animals) { animal in
-                    NavigationLink(destination:AnimalDetailView(animal: animal)){
-                        AnimalListView(animal: animal)
+            Group{
+                if !isGridActive {
+                    List {
+                        CoverImageView(images: coverImages)
+                            .frame(height:300)
+                            .listRowInsets(.none)
+                        ForEach(animals) { animal in
+                            NavigationLink(destination:AnimalDetailView(animal: animal)){
+                                AnimalListView(animal: animal)
+                            }
+                        }
                     }
+                } else {
+                    BrowseViewGrid(gridLayoutColumns: gridType.rawValue)
+                        .padding()
                 }
             }
             .navigationBarTitle("Real Africa")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    HStack {
+                        Button(action:{
+                            isGridActive = false
+                            impact.impactOccurred()
+                        }){
+                            Image(systemName:"square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridActive ? .primary: .accentColor )
+                        }
+                        
+                        Button(action:{
+                            switchGrid()
+                        }){
+                            Image(systemName:gridImageName)
+                                .font(.title2)
+                                .foregroundColor(
+                                    isGridActive ?
+                                        .accentColor : .primary
+                                )
+                        }
+                        
+                    }
+                }
+            }
         }
     }
 }
---
+++ Real Africa/Views/BrowseViewGrid.swift
@@ -0,0 +1,39 @@
+//
+//  BrowseViewGrid.swift
+//  Real Africa
+//
+//  Created by Arsalan majlesi on 7/17/21.
+//
+
+import SwiftUI
+
+struct BrowseViewGrid: View {
+    var gridLayoutColumns : Int
+    @State var gridLayout : [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    func getCurrentGridLayout() -> [GridItem] {
+        return Array(repeating: GridItem.init(.flexible()), count: gridLayoutColumns)
+    }
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns:getCurrentGridLayout(),spacing:10) {
+                ForEach(ANIMALS) { animal in
+                    Image(animal.image)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(12)
+                        
+                }
+        }
+        }
+        .onAppear(){
+            gridLayout = Array(repeating: GridItem(.flexible()), count: gridLayoutColumns)
+        }
+    }
+}
+
+struct BrowseViewGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        BrowseViewGrid(gridLayoutColumns: 3)
+    }
+}
